5dd8193bc37b5a09b875c11004c2e11eb6995702
AvdhootJadhav <68023630+AvdhootJadhav@users.noreply.github.com>
2022-11-06 12:30:45 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyANTNay5L341nGRwvZvr_DR2OhIWfJHvH4",
-  authDomain: "whatsapp-clone-1371c.firebaseapp.com",
-  projectId: "whatsapp-clone-1371c",
-  storageBucket: "whatsapp-clone-1371c.appspot.com",
-  messagingSenderId: "160995441388",
-  appId: "1:160995441388:web:60f9ec8dc04d6de3b143c9"
+  apiKey: "your_key",
+  authDomain: "your_id.firebaseapp.com",
+  projectId: "your_id",
+  storageBucket: "your_bucket",
+  messagingSenderId: "your_id",
+  appId: "your_fb_id"
 };
 
 const app = initializeApp(firebaseConfig);
